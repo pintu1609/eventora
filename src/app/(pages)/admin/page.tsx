509aee4bca2cc03bce8fb510dfdbc7c3c2778975
Pintu <pintu@Mac.lan>
2025-06-08 12:57:03 +0530

    fix css

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -37,7 +37,7 @@ export default function Admin() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-120px)] bg-gradient-to-br from-blue-50 to-purple-100 px-4">
       <div className="bg-white p-8 rounded-lg shadow-2xl w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Admin Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Admin Login</h2>
         <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
           <label className="flex flex-col gap-2 text-left text-lg text-gray-600 font-semibold">
             Username:
